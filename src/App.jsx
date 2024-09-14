36e463794bc6c47e8547f20645008ffc70ad087d
NguyenHao <Henryporison@outlook.com.vn>
2024-09-14 22:21:00 +0700

    uploadcode

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './styles/main.scss';
+import SideNav from './Components/side_Nav/SideNav.jsx';
+
+function App() {
+  return (
+    <div className="app">
+      <SideNav />
+      <div className="content">
+        {/* Phần nội dung chính */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
