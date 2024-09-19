1fae48fccd8c670830cd4d9efec0b74463657514
NguyenHaoTran <henryporison@outlook.com.vn>
2024-09-19 23:49:02 +0700

    19.9

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,7 +6,10 @@ const Home = () => {
     <div className="home_container">
       <div className="contents">
         <div className="name">
-          <h1>TRẦN NGUYÊN <br/><span>HÀO</span></h1>
+          <h1>
+            TRẦN NGUYÊN <br />
+            <span>HÀO</span>
+          </h1>
           <p>
             frontend devoloper <i class="ri-code-s-slash-line"></i>
           </p>
@@ -22,10 +25,55 @@ const Home = () => {
       {/* projects */}
       <div className="projects">
         <h2>Projects</h2>
-        <div className="projects_card">
-          
+        <div className="cards">
+          {/* Card */}
+          <div className="card">
+            <div class="content">
+              <h2>001</h2>
+              <h3>Card One</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                ea sapiente fuga, animi magnam earum.
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+          <div className="card">
+            <div class="content">
+              <h2>001</h2>
+              <h3>Card One</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                ea sapiente fuga, animi magnam earum.
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+          <div className="card">
+            <div class="content">
+              <h2>001</h2>
+              <h3>Card One</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                ea sapiente fuga, animi magnam earum.
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+          <div className="card">
+            <div class="content">
+              <h2>001</h2>
+              <h3>Card One</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+                ea sapiente fuga, animi magnam earum.
+              </p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
         </div>
       </div>
+      {/*Skills */}
     </div>
   );
 };
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,10 +1,23 @@
 import React from "react";
-import "./projects.scss"
+import "./projects.scss";
 
 const Projects = () => {
   return (
     <div className="projects_container">
       <h1>Projects</h1>
+      <div className="cards">
+        <div className="card">
+          <div class="content">
+            <h2>001</h2>
+            <h3>Card One</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+              ea sapiente fuga, animi magnam earum.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
