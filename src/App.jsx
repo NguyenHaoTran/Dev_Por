2a215868a000264d97418bf3ce15d5d9daa6356e
NguyenHao <Henryporison@outlook.com.vn>
2024-09-24 20:53:55 +0700

    final_24/9

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,21 @@ import KinhNghiem from "./Components/KinhNghiem/KinhNghiem.jsx"
 import Home from "./Components/Home/Home.jsx";
 import Projects from "./Components/Projects/Projects.jsx";
 import Skills from "./Components/Skills/Skills.jsx";
+import { useEffect } from 'react';
 // remix icons
 import 'remixicon/fonts/remixicon.css';
 // 
 
 
 function App() {
+  useEffect(() => {
+    const favicon = document.getElementById('dynamic-favicon');
+    
+    if (favicon) {
+      favicon.href = '/Gif.gif';
+    }
+  }, []);
+
   return (
     <>
         <Router>
